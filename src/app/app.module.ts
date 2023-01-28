7b392acc3c04de28b4bde66826e2848dbf6f71e7
Tomas Maggi <tm82945g@gmail.com>
2023-01-28 16:17:21 -0300

    add upper-navigation component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UpperMenuComponent } from './components/upper-menu/upper-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UpperMenuComponent
   ],
   imports: [
     BrowserModule
