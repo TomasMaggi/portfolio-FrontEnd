d6e32af20be55c1c2dc15d47bd2609baab082c08
Tomas Maggi <tm82945g@gmail.com>
2023-01-28 16:21:12 -0300

    move main div to main-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { UpperMenuComponent } from './components/upper-menu/upper-menu.component';
+import { MainContentComponent } from './components/main-content/main-content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UpperMenuComponent
+    UpperMenuComponent,
+    MainContentComponent
   ],
   imports: [
     BrowserModule
