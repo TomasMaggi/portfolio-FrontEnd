e92839995f958177add4570e27c776cccb842d64
Tomas Maggi <tm82945g@gmail.com>
2023-02-11 21:44:04 -0300

    add feature to add new education component

--- src/app/inner-components/education/education.component.ts
+++ src/app/inner-components/education/education.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Input } from '@angular/core';
 import { GetDataloadService } from 'src/app/services/get-dataload/get-dataload.service';
 import { IEducation } from 'src/app/interfaces/education_interface';
 import { DatePipe } from '@angular/common';
+import { ComponentGeneratorService } from 'src/app/services/component-generator/component-generator.service';
+import { IsLoadingService } from 'src/app/services/is-loading/is-loading.service';
 
 @Component({
   selector: 'app-education',
@@ -14,7 +16,15 @@ export class EducationComponent implements OnInit {
   @Input()
   isLogged: boolean = false;
 
-  constructor(private getdataservice: GetDataloadService) {}
+  adding = false;
+  new_start_date: Date | null = null;
+  new_finish_date: Date | null = null;
+
+  constructor(
+    private getdataservice: GetDataloadService,
+    private newComponentService: ComponentGeneratorService,
+    private loadingMessage: IsLoadingService
+  ) {}
 
   ngOnInit(): void {
     this.fill();
@@ -29,5 +39,23 @@ export class EducationComponent implements OnInit {
       .subscribe((data: any) => {
         this.dataload = data;
       });
+  }
+  save(title: string, type: string, institution: string, desc: string) {
+    const data = {
+      title: title,
+      type: type,
+      institution: institution,
+      description: desc,
+      date_of_start: this.new_start_date,
+      date_of_finish: this.new_finish_date,
+    };
+    this.loadingMessage.sendData(false);
+    this.newComponentService.add('education/', data).subscribe(() => {
+      window.location.reload();
+    });
+  }
+
+  addDate(e: any) {
+    return new Date(e.target.value);
   }
 }
